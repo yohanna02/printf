6cb8b24bb641e5e3b8be5493d1c573be0c85459e
Raymondamem <raymondamem525@gmail.com>
2023-04-25 21:49:07 +0100

    added for binary printing

--- print_binary.c
+++ print_binary.c
@@ -6,7 +6,7 @@ int print_binary(va_list arg_list)
 	char c;
 
 	n = va_arg(arg_list, int);
-	for (i = 7; i >= 0; i--)
+	for (i = 6; i >= 0; i--)
 	{
 		c = (n & (1 << i)) ? '1' : '0';
 		count += _putchar(c);
