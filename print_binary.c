873f0c38213944f625819e98e1b22135c1db449e
Raymondamem <raymondamem525@gmail.com>
2023-04-27 12:02:17 +0100

    fixed _printf function to use 32 bit

--- print_binary.c
+++ print_binary.c
@@ -1,32 +1,17 @@
 #include "main.h"
 
-/**
- * print_binary_reursion - print number to binary
- * @n: va argument list
- * Return: return binary string
- */
-int print_binary_reursion(unsigned int n)
-{
-	int count = 0;
-
-	if (n > 1)
-		print_binary_reursion(n >> 1);
-
-	count += _putchar((n & 1) ? '1' : '0');
-
-	return (count);
-}
-
-/**
- * print_binary - print number to binary
- * @arg_list: va argument list
- * Return: return binary string
- */
 int print_binary(va_list arg_list)
 {
-	int n;
+	int count = 0, i;
+	unsigned int n;
+	char c;
 
 	n = va_arg(arg_list, int);
-
-	return (print_binary_reursion(n));
+	for (i = 31; i >= 0; i--)
+	{
+		c = (n & (1 << i)) ? '1' : '0';
+		count += _putchar(c);
+	}
+	
+	return (count);
 }
